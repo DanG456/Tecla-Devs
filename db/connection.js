c7300c140e72a841d7fac636747d89cbbba0a8c2
DanG456 <80288370+DanG456@users.noreply.github.com>
2021-09-07 19:31:07 -0500

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -4,10 +4,20 @@ const {DataTypes, Models} = require('sequelize')
 
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-    dialect: 'mssql',
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT
-})
+const sequelize = new Sequelize(process.env.DB_NAME, null, null, {
+    dialect: "mssql",
+    server: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialectOptions: {
+        authentication: {
+            type: 'default',
+            options: {
+                encrypt: true,
+                userName: process.env.DB_USER,
+                password: process.env.DB_PASS,
+            }
+        },
+    }
+  });
 
 module.exports = sequelize
